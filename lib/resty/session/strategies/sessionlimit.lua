8cdfff5e27e543d1fcdb3d270c91c65f9aec9db5
Jesse Cooper <jesse.cooper@sciencelogic.com>
2020-12-29 10:01:24 -0600

    add sessionlimit to strategies

---
+++ lib/resty/session/strategies/sessionlimit.lua
@@ -0,0 +1,70 @@
+local tonumber = tonumber
+local default = require "resty.session.strategies.default"
+local regenerate = require "resty.session.strategies.regenerate"
+
+local concat  = table.concat
+
+local defaults = {
+    concurrent_limit = tonumber(ngx.var.session_concurrent_limit) or 0
+}
+
+local strategy = {
+  start   = regenerate.start,
+  destroy = regenerate.destroy,
+  close   = regenerate.close,
+  open    = regenerate.open,
+  touch   = regenerate.touch
+}
+
+local function key(source)
+  if source.usebefore then
+    return concat{ source.id, source.usebefore }
+  end
+
+  return source.id
+end
+
+local function check_open_sessions(session)
+    -- check for current user session
+    if defaults.concurrent_limit > 0 then
+        ngx.log(ngx.DEBUG, "concurrent limit being checked...")
+        if session.data and session.data.user then
+            ngx.log(ngx.DEBUG, "user email: " .. session.encoder.encode(session.data.user.email))
+            -- take encoded email and slice to -2 to account for the padding flipping the last char exclude locks
+            -- TODO: could the -2 chars on the hash cause collisions? needs exploration
+            local encoded_name = session.encoder.encode(session.data.user.email)
+            local res = session.storage:scan(string.sub(encoded_name, 1, #encoded_name-2) .. "*[^.lock]")
+            if res and #res[2] >= defaults.concurrent_limit then
+                ngx.log(ngx.ERR, "session limit reached for user: " .. session.data.user.email)
+                return nil
+            end
+            ngx.log(ngx.DEBUG, "Limit not reached for user ID: " .. session.data.user.email .. " res: " .. type(res))
+            return true
+        end
+    end
+    ngx.log(ngx.DEBUG, "Session limits not configured")
+    return true
+end
+
+function strategy.save(session, close)
+    local storage = session.storage
+    ngx.log(ngx.DEBUG, "Strategy save process...")
+    if session.present then
+        if storage.ttl then
+            storage:ttl(session.encoder.encode(session.id), session.cookie.discard, true)
+        elseif storage.close then
+            storage:close(session.encoder.encode(session.id))
+        end
+
+        session.id = session:identifier()
+    end
+    if check_open_sessions(session) then
+        ngx.log(ngx.DEBUG, "Saving session")
+        return default.modify(session, "save", close, key(session))
+    else
+        ngx.log(ngx.STDERR, "Session limit reach session can not be saved")
+        return nil
+    end
+end
+
+return strategy
--- lua-resty-session-dev-1.rockspec
+++ lua-resty-session-dev-1.rockspec
@@ -27,6 +27,7 @@ build = {
         ["resty.session.storage.memcached"]     = "lib/resty/session/storage/memcached.lua",
         ["resty.session.strategies.default"]    = "lib/resty/session/strategies/default.lua",
         ["resty.session.strategies.regenerate"] = "lib/resty/session/strategies/regenerate.lua",
+        ["resty.session.strategies.sessionlimit"] = "lib/resty/session/strategies/sessionlimit.lua",
         ["resty.session.hmac.sha1"]             = "lib/resty/session/hmac/sha1.lua",
         ["resty.session.ciphers.aes"]           = "lib/resty/session/ciphers/aes.lua",
         ["resty.session.ciphers.none"]          = "lib/resty/session/ciphers/none.lua",
