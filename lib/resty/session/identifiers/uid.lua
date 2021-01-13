ea4f219d30cad7f3f72c54d0705da8218d52db0f
Jesse Cooper <jesse.cooper@sciencelogic.com>
2021-01-13 14:11:35 -0600

    modify log messaging for auditing sessions

--- lib/resty/session/identifiers/uid.lua
+++ lib/resty/session/identifiers/uid.lua
@@ -1,19 +1,23 @@
 local random   = require "resty.random".bytes
 
 local defaults = {
-    length = 6
+    length = 10
 }
 
 return function(session)
-    -- local config = session or defaults
-    -- local length = tonumber(config.length, 10) or defaults.length
     local length = defaults.length
-    -- local uid = ngx.req.get_headers()[defaults.header_name] or ''
     local uid = ''
+    local pad = random(length, true) or random(length)
     if session.data and session.data.user then
         uid = session.data.user.email
+        local id_encoded = session.encoder.encode(uid .. pad)
+        ngx.log(
+                ngx.WARN, "session id: " .. id_encoded .. " created for: " .. uid ..
+                        " from session " .. session.encoder.encode(session.id)
+        )
+    else
+        ngx.log(ngx.WARN, "anonymous session id: " .. session.encoder.encode(pad) .. " create for non authenticated user")
     end
-    local pad = random(length, true) or random(length)
     ngx.log(ngx.DEBUG, "uid: " .. uid .. " pad: " .. pad)
 
     -- return uid if avalible with padding
